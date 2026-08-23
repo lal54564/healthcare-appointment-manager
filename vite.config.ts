a4e5735adf086f89990237e2c7050479aa8036bd
lal54564 <lalganesh2190@gmail.com>
2026-08-23 15:12:59 +0530

    Convert to pure SPA mode for flawless Vercel static deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    tanstackStart(),
+    tanstackStart({
+      spa: {
+        enabled: true,
+      },
+    }),
     react(),
   ],
 });
